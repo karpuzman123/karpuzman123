a9d2d9f7ada14595902467f2f099281312553404
karpuzman123 <a12943089@gmail.com>
2024-09-20 21:32:43 +0300

    Add files via upload

---
+++ SADECE  ŞAKA AMAÇLI VİRÜSLER/şaka.bat
@@ -0,0 +1,58 @@
+@echo off
+
+
+md virüs 1
+md virüs 2
+md virüs 3
+md virüs 4
+md virüs 5
+md virüs 0
+md virüs 1
+md virüs 2
+md virüs 3
+md virüs 4
+md virüs 5
+md virüs 6
+md virüs 7
+md virüs 8
+md virüs 9
+md virüs 10
+md virüs 11
+md virüs 12
+md virüs 13
+md virüs 14
+md virüs 15
+md virüs 16
+md virüs 17
+md virüs 18
+md virüs 19
+md virüs 20
+md virüs 21
+md virüs 22
+md virüs 23
+md virüs 24
+md virüs 25
+md virüs 26
+md virüs 27
+md virüs 28
+md virüs 29
+md virüs 30
+md virüs 31
+
+echo pn gg yok
+
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
+START
